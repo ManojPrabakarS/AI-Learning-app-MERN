b785d23ba5564d77cd00cfdc007b920fe896c34e
Manoj Prabakar <manojprabakar20022000@gmail.com>
2026-03-10 19:05:09 +0530

    Update MarkDownRender.jsx

--- frontend/src/components/common/MarkDownRender.jsx
+++ frontend/src/components/common/MarkDownRender.jsx
@@ -2,7 +2,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { Children } from 'react';
+
 
 const MarkDownRender = ({ content }) => {
     return (
